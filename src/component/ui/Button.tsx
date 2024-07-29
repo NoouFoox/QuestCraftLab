168eedd60f41fe8626119ec8e8aab0d73af15e8e
贺俊浩 <nougat_hx@qq.com>
2024-07-29 17:15:38 +0800

    chore: Update Button component to support custom styling and add children prop

--- src/component/ui/Button.tsx
+++ src/component/ui/Button.tsx
@@ -2,7 +2,27 @@ import {
   Button as Hbutton,
   ButtonProps as HButtonProps,
 } from "@headlessui/react";
-interface ButtonProps extends HButtonProps {}
+import clsx from "clsx";
+import { ReactNode } from "react";
+import { Fragment } from "react/jsx-runtime";
+interface ButtonProps extends HButtonProps {
+  children: ReactNode;
+}
 export default function Button(props: ButtonProps) {
-  return <Hbutton {...props} />;
+  return (
+    <Hbutton as={Fragment} {...props}>
+      {({ hover, active }) => (
+        <button
+          className={clsx(
+            "rounded py-2 px-4 text-sm text-white",
+            !hover && !active && "bg-sky-600",
+            hover && !active && "bg-sky-500",
+            active && "bg-sky-700"
+          )}
+        >
+          {props.children}
+        </button>
+      )}
+    </Hbutton>
+  );
 }
