fe9b4684771ea05d7ec39ad451c591d479944996
贺俊浩 <nougat_hx@qq.com>
2024-07-31 17:31:07 +0800

    chore: Update npm dependencies and add js-yaml library

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,11 +1,22 @@
 import ThemeButton from "@/component/system/ThemeButton";
 import Button from "@/component/ui/Button";
 import { t } from "i18next";
-
+import yaml from "js-yaml";
+const JSONdata = {
+  title: "任务",
+  npcs: [1, 2, 3],
+  ops: {
+    add: 1,
+  },
+}
+const YMLdata = yaml.dump(JSONdata);
 export default function Home() {
+  console.log(YMLdata);
+  console.log(yaml.load(YMLdata));
+
   return (
     <div>
-      <h1>{t('welcome')}</h1>
+      <h1>{t("welcome")}</h1>
       <Button>Click me</Button>
       <ThemeButton />
     </div>
