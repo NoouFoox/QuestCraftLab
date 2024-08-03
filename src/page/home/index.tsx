f7edcd9d996c988b8fbd1935c8065ff6c9da9da4
贺俊浩 <nougat_hx@qq.com>
2024-08-03 17:08:03 +0800

    ```text chore: Update language files for zh-CN and zh-HK
    This commit updates the language files for zh-CN and zh-HK to include the "add" quest option in the "system" menu. This ensures that the menu displays the correct label for these languages.

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,6 +1,7 @@
 import yaml from "js-yaml";
 import HomeCard from "./components/home-card";
 import { Plus } from "lucide-react";
+import { useTranslation } from "react-i18next";
 const JSONdata = {
   name: {
     title: "任务",
@@ -17,11 +18,12 @@ const JSONdata = {
 };
 const YMLdata = yaml.dump(JSONdata);
 export default function Home() {
+  const { t } = useTranslation();
   return (
     <div className="grid gap-4 p-4 md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
       <HomeCard className="flex justify-center items-center flex-col gap-1 cursor-pointer border-dashed">
         <Plus />
-        新增任务
+        <span>{t("system.add.quest")}</span>
       </HomeCard>
       {new Array(30).fill(null).map((_, index) => (
         <HomeCard key={index}>
